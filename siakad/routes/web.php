71a1f3c2c90f56b6cee6331ad7f1db88157e0ad3
Steven Line <stevenline12@gmail.com>
2026-04-12 14:54:58 +0700

    Add route for creating and deleting user

--- siakad/routes/web.php
+++ siakad/routes/web.php
@@ -5,18 +5,21 @@ use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function(){
-        
-    Route::get('/home', function() {
-        return view('home');
-    });
+  
+ 
     Route::delete('/logout', [LoginController::class, 'destroy']);
     Route::get('/admin', function(){
-        return view('admin.dashboard');
-    });
+        return view('admin.dashboard'); });
     Route::get('/admin/kelola-user', [UserController::class, 'index']);
     Route::get('/admin/kelola-user/create', [UserController::class, 'create']);
+    Route::post('/admin/kelola-user/create', [UserController::class, 'store']);
+    Route::delete('/admin/kelola-user/{user}', [UserController::class, 'destroy']);
+    
 });
 Route::middleware('guest')->group(function(){
+    Route::get('/', function(){
+        return redirect('/login');
+    });    
     Route::get('/login', [LoginController::class, 'create'])->name('login');
     Route::post('/login', [LoginController::class, 'store']);
 });
