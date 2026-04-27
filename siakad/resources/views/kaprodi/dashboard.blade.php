9255cf070a17b06bcd5409749a8a186694f2d55d
Steven Line <stevenline12@gmail.com>
2026-04-27 09:01:28 +0700

    Fix Commit Issue

--- siakad/resources/views/kaprodi/dashboard.blade.php
+++ siakad/resources/views/kaprodi/dashboard.blade.php
@@ -1,21 +1,10 @@
 <x-layout>
-<<<<<<< HEAD
-        <h1 class="text-2xl font-bold mb-6">Dashboard Kaprodi</h1>
-
-    <!-- Statistik -->
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-6 mb-6">
-        
-        <div class="bg-white shadow rounded-xl p-5">
-            <h2 class="text-gray-500 text-sm">Total Mahasiswa</h2>
-            <p class="text-2xl font-bold mt-2">120</p>
-=======
 <div class="flex flex-col h-full justify-between bg-gray-900 p-4">
 
     <!-- ATAS -->
     <div>
         <div class="mb-6 text-white font-bold text-lg">
             Kaprodi Menu
->>>>>>> 5801e2cf1bb2996a8542a69e2cce7114f9236356
         </div>
 
         <ul class="space-y-3">
