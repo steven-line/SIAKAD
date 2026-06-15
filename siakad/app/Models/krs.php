5d5ac5f225b571ac5d66f2f234921ec652e4fdfe
Steven2425 <albertosteven240@gmail.com>
2026-06-15 14:44:46 +0700

    progress 15 juni

--- siakad/app/Models/krs.php
+++ siakad/app/Models/krs.php
@@ -6,12 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Krs extends Model
 {
+    //
     protected $table = 'krs';
-    public $timestamps = false;
-    protected $primaryKey = null; // tidak ada primary key tunggal
-    public $incrementing = false;
-
-    protected $fillable = [
-        'NRP', 'KODE', 'BU', 'TTT1', 'TTT2', 'TTT3', 'LAIN', 'UTS', 'UAS', 'NA', 'SKS', 'PERIODE', 'KELAS', 'SURVEY'
-    ];
+     public $timestamps = false;
+     protected $fillable = [
+        'nrp',
+        'kode',
+        'bu',
+        'ttt1',
+        'ttt2',
+        'ttt3',
+        'lain',
+        'uts',
+        'uas',
+        'na',
+        'sks',
+        'periode',
+        'kelas',
+        'survey'
+        ];
 }
