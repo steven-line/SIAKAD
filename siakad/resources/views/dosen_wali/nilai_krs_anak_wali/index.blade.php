61c3f106f3cddde562647036653e38aeb6be9699
Steven Line <stevenline12@gmail.com>
2026-06-20 08:43:09 +0700

    Membuat views untuk nilai krs anak wali

---
+++ siakad/resources/views/dosen_wali/nilai_krs_anak_wali/index.blade.php
@@ -0,0 +1,72 @@
+<x-layout title="Daftar Mahasiswa Anak Wali">
+
+    <a class="join-item btn btn-primary mb-10" href="{{ url()->previous() }}">
+        ⮜ Previous page
+    </a>
+
+    <div class="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+
+        <table class="table">
+
+            <thead class="bg-blue-500 text-white">
+                <tr>
+                    <th>No</th>
+                    <th>NRP</th>
+                    <th>Nama Mahasiswa</th>
+                    <th>Program Studi</th>
+                    <th>Dosen Wali</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+
+            <tbody>
+
+                @forelse($mahasiswas as $mhs)
+
+                    <tr>
+
+                        <td>{{ $loop->iteration }}</td>
+
+                        <td>{{ $mhs->nrp }}</td>
+
+                        <td>{{ $mhs->biodata->nama }}</td>
+
+                        <td>{{ $mhs->prodi }}</td>
+
+                        <td>{{ $mhs->dosen_wali }}</td>
+
+                        <td>
+
+                            <a
+                                class="btn btn-soft btn-info"
+                                href="">
+
+                                Lihat Nilai
+
+                            </a>
+
+                        </td>
+
+                    </tr>
+
+                @empty
+
+                    <tr>
+                        <td colspan="6" class="text-center">
+                            Tidak ada mahasiswa anak wali.
+                        </td>
+                    </tr>
+
+                @endforelse
+
+            </tbody>
+
+        </table>
+
+    </div>
+
+    <div class="mt-5">
+        {{ $mahasiswas->links() }}
+    </div>
+
+</x-layout>
