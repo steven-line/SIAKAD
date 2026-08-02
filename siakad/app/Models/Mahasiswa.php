65c6707e8121ecdf83bb0e702cea159e1ab207e4
Michael Setiawan <maikelstwan@gmail.com>
2026-08-02 19:31:27 +0700

    Cuma commit aja, belum ada update

--- siakad/app/Models/Mahasiswa.php
+++ siakad/app/Models/Mahasiswa.php
@@ -8,12 +8,6 @@ use App\Models\Dosen;
 use App\Models\Biodata;
 use App\Models\Ips;
 
-use App\Models\Biodata;
-use App\Models\Dosen;
-use App\Models\Registrasi;
-use App\Models\Prodi;
-use App\Models\Ips;
-
 class Mahasiswa extends Model
 {
     //
